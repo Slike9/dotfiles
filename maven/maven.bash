bfe3786d1d68f54f95261304142ed4f81e7fe4dc
Sergey Semyonov <slike9@rambler.ru>
2014-04-11 00:06:35 +0400

    Init commit

---
+++ maven/maven.bash
@@ -0,0 +1,6 @@
+# Maven
+export M2_HOME=/opt/apache-maven-3.2.1
+export M2=$M2_HOME/bin
+#export MAVEN_OPTS="-Xms256m -Xmx512m"
+PATH=$M2:$PATH
+
---
+++ ruby/spring.bash
@@ -0,0 +1,1 @@
+export SPRING_TMP_PATH=/home/slike9/spring_tmp
