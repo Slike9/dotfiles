e0d939ff4bac43d06b1007ba32f9dece6861040d
Sergey Semyonov <sergey.semenov@workato.com>
2025-04-09 20:21:37 +0400

    nvim: Set clipboard option

--- nvim.config/lua/config/lazy.lua
+++ nvim.config/lua/config/lazy.lua
@@ -21,6 +21,8 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " "
 vim.g.maplocalleader = ","
 
+vim.opt.clipboard = "unnamedplus" -- Sync clipboard between OS and Neovim
+
 -- Setup lazy.nvim
 require("lazy").setup({
   spec = {
