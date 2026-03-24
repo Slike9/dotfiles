653c15ba1c56b5134140357448302488bbcc653e
Sergey Semyonov <sergey.semenov@workato.com>
2026-03-24 17:09:52 +0400

    Update neovim keymaps

--- nvim.config/lua/plugins.lua
+++ nvim.config/lua/plugins.lua
@@ -368,7 +368,7 @@ return {
       keymap = {
         preset = 'default',
 
-        -- ['<C-k>'] = { 'select_prev', 'fallback_to_mappings' },
+        ['<C-k>'] = { 'select_prev', 'fallback_to_mappings' },
         ['<C-j>'] = { 'select_next', 'fallback_to_mappings' },
 
         ['<Tab>'] = {
