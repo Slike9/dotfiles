e764bebb6a5c555326654eec76d19d202b704c11
Sergey Semyonov <sergey.semenov@workato.com>
2026-03-30 14:50:51 +0400

    nvim: Use nvim-surround plugin instead of vim-surround

--- nvim.config/lua/plugins.lua
+++ nvim.config/lua/plugins.lua
@@ -96,7 +96,20 @@ return {
 
   'tpope/vim-abolish',
   'tpope/vim-repeat',
-  'tpope/vim-surround',
+
+  -- 'tpope/vim-surround',
+  {
+    "kylechui/nvim-surround",
+    -- version = "^4.0.0", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    -- Optional: See `:h nvim-surround.configuration` and `:h nvim-surround.setup` for details
+    -- config = function()
+    --     require("nvim-surround").setup({
+    --         -- Put your configuration here
+    --     })
+    -- end
+  },
+
   'tpope/vim-eunuch',
   'tpope/vim-unimpaired',
 
